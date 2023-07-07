9f7accfa108913c2bd69f158a833ce4f17cfcf8c
Riddhesh Naik <riddheshnaik786@gmail.com>
2023-07-07 16:58:46 +0530

    international done commit

--- src/app/destinations/destinations-international/destinations-international.component.ts
+++ src/app/destinations/destinations-international/destinations-international.component.ts
@@ -1,4 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { InternationalTour } from 'src/app/models/internationalTourModel';
+import { NationalTour } from 'src/app/models/nationalTourModel';
+import { DestinationsService } from 'src/app/services/destinations.service';
 
 @Component({
   selector: 'app-destinations-international',
@@ -6,5 +12,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./destinations-international.component.css']
 })
 export class DestinationsInternationalComponent {
+
+  public internationalTours: InternationalTour[];
+
+  public internationalTour$:any
+
+  selectedId: number;
+
+  constructor(
+    private service: DestinationsService,
+    private route:ActivatedRoute
+    
+  ) {}
+
+  ngOnInit(): void {
+    this.getInterNationalTours(); 
+
+    this.internationalTour$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        this.selectedId = Number(params.get('id'));
+        console.log(this.selectedId);
+        return this.service.getInternationalTours();
+      })
+    );
+
+   }
+
+   public getInterNationalTours(): void {
+    this.service.getInternationalTours().subscribe(
+      (response: InternationalTour[]) => {
+        this.internationalTours = response;
+        console.log(this.internationalTours)
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
 
 }
--- src/app/destinations/view-international/view-international.component.ts
+++ src/app/destinations/view-international/view-international.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router,ActivatedRoute, Params } from '@angular/router';
+import { InternationalTour } from 'src/app/models/internationalTourModel';
+import { NationalTour } from 'src/app/models/nationalTourModel';
+import { DestinationsService } from 'src/app/services/destinations.service';
 
 @Component({
   selector: 'app-view-international',
@@ -6,5 +11,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-international.component.css']
 })
 export class ViewInternationalComponent {
+
+  public internationalTours: InternationalTour[];
+
+  selectedId: number;
+
+  // public trek: Trek[];
+
+  internationalTour: any;
+
+  id: number;
+
+  constructor(
+    private router: Router,
+    private service: DestinationsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getInterNationalTours();
+
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      console.log(this.id);
+    });
+    this.getInterNationalTour(this.id);
+  
+    
+  }
+
+  public getInterNationalTour(id: number) {
+    this.service.getInternationalTour(this.id).subscribe(
+      (data) => {
+        console.log(data);
+        this.internationalTour = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  public getInterNationalTours(): void {
+    this.service.getInternationalTours().subscribe(
+      (response: InternationalTour[]) => {
+        this.internationalTours = response;
+        console.log(this.internationalTours)
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public getAllInterNationalTours() {
+    this.getInterNationalTours();
+  }
+
+  public goBackToInterNationalTours() {
+    this.router.navigate(['/destinations/international']);
+  }
+
+
 
 }
